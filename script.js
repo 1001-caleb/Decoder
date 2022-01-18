6acebf66372bb2b5b87429eb8622ec193dff03b4
1001-caleb <71718050+1001-caleb@users.noreply.github.com>
2022-01-18 16:24:01 -0500

    add funtion encrypt and decrypt

--- script.js
+++ script.js
@@ -10,11 +10,31 @@ No se permite acentuación de palabras
 */
 
 /* Reglas de desencriptación: 
-"enter" es convertido para "e" 
-"imes" es convertido para "i"
-"ai" es convertido para "a"
-"ober" es convertido para "o"
-"ufat" es convertido para "u"
-Solo letras minusculas
-No se permite acentuación de palabras   
+lo mismo pero al revés. 
 */
+
+// function cambiarMensajeResultado(){
+//     var msg = document.getElementById("result__msg").value;
+//     msg = document.writeln("Este es el resultado: ")
+// }
+
+function botonEncriptar(){
+    const encriptar = document.getElementById('textoAEncriptar').value;
+    const encriptado = encriptar.replace(/(e)/gi, 'enter').replace(/(i)/gi, 'imes').replace(/(a)/gi, 'ai').replace(/(o)/gi, 'ober').replace(/(u)/gi, 'ufat');
+    document.getElementById("textoEncriptado").value = encriptado
+    document.getElementById("textoAEncriptar").value=" ";
+    // cambiarMensajeResultado();
+}
+
+var button = document.getElementById("btnEncriptar");
+button.onclick = botonEncriptar;
+
+function botonDesencriptar(){
+    const desencriptar = document.getElementById('textoAEncriptar').value;
+    const desencriptado = desencriptar.replace(/(enter)/gi, 'e').replace(/(imes)/gi, 'i').replace(/(ai)/gi, 'a').replace(/(ober)/gi, 'o').replace(/(ufat)/gi, 'u');
+    document.getElementById("textoEncriptado").value = desencriptado
+    document.getElementById("textoAEncriptar").value=" ";
+}
+
+var button2 = document.getElementById("btnDesencriptar");
+button2.onclick = botonDesencriptar;
